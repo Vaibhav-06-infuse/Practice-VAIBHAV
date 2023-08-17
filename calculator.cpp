5eb485137926e22f2effb7fd2ac4ffa4808aa6d0
Vaibhav-06-infuse <vaibhav.gaunkar@infuse.it>
2023-08-17 14:32:41 +0530

    Revert "commit remaining changes"
    This reverts commit 0cd8ed83ae3aefcda9bb2b4aba28e58ec96c56a3.

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 using namespace std;
 
+// Function to add two numbers
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    int sum = add(num1, num2);
+
+    cout << "Sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+
+    return 0;
+}
+In this program, the add function takes two integer parameters (a and b) and returns their sum. In the main function, the user is prompted to input two numbers, and then the add function is called to calculate the sum, which is then displayed on the screen.
+
+Compile and run this program using a C++ compiler to see the result.
+
+
+
+
+User
+make a cpp class which contain a add funtion to add tow int
+ChatGPT
+Certainly! Here's an example of a C++ class that contains an add function to add two integers:
+
+cpp
+Copy code
+#include <iostream>
+using namespace std;
+
 class MathCalculator {
 public:
     // Function to add two integers
@@ -10,10 +47,6 @@ public:
 
     int sub(int a,int b){
         return a - b;
-    }
-
-    int mul(int a,int b){
-        return a * b;
     }
 };
 
